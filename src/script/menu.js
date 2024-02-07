150cc47db8a7a588771545e82ecc352fc6415bfd
g3deon <123526476+katia-tsx@users.noreply.github.com>
2024-02-07 18:24:58 -0400

    menu de hamburguesa, cambio en el archivo principal, about y blog, también el responsibe para telefonos

---
+++ src/script/menu.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.hamburger').addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.toggle('expanded');
+    });
+});
